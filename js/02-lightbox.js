cddb64ca900cb91cbd079309a77f26e59ed733ff
Anton949432 <133233466+Anton949432@users.noreply.github.com>
2023-09-12 03:10:35 +0300

    Add files via upload

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,25 +3,26 @@ import { galleryItems } from './gallery-items.js';
 const galleryContainer = document.querySelector('.gallery');
 
 const galleryMarkup = galleryItems
-    .map(item => `
+  .map(item => `
     <li class="gallery__item">
       <a class="gallery__link" href="${item.original}">
         <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
       </a>
     </li>
   `)
-    .join('');
+  .join('');
 
 galleryContainer.innerHTML = galleryMarkup;
 
 const lightbox = new SimpleLightbox('.gallery a', {
-    captions: true,
-    captionPosition: 'bottom',
-    captionDelay: 250,
+  captions: true,
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  captionsData: 'alt',
 });
 
 galleryContainer.addEventListener('click', (event) => {
-    event.preventDefault();
+  event.preventDefault();
 });
 
 
